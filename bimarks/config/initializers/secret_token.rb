b9daf58f82173f35a9ac1bde03e9efbda33dad3f
Ioannis Gakos <gakos.ioannis@gmail.com>
2011-05-07 20:31:50 +0300

    Switching to Rails. Get it started!

---
+++ bimarks/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bimarks::Application.config.secret_token = 'daaad91b6560d4e44a2dab2c20f7dd8b2b93d3a274254e158cdc84ab935dc3c7d02e7b7ce0f6211316ab209a4b88a7cebd9693f0258ec1a38b45a61cf5deec28'
