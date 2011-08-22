9a4a01dbb10d2b91ba9372236dd574f0154b6fc5
Ioannis Kotsogiannis <kotsogiann@ceid.upatras.gr>
2011-08-22 23:18:07 +0300

    Ok I fixed the home_controller to show the most recent 10 bookmarks, and not the least recent.

--- bimarks/app/controllers/home_controller.rb
+++ bimarks/app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @bookmarks_most_recent = Bookmark.all.last(10)
+    @bookmarks_most_recent = Bookmark.all.first(10)
     @bookmarks_most_rated = Bookmark.all
      respond_to do |format|
 
