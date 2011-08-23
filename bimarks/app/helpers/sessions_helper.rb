d5e07c06422bc496f70407d222c85d18b55cb3e7
Ioannis Kotsogiannis <kotsogiann@ceid.upatras.gr>
2011-08-23 06:32:22 +0300

    Just polished a little bit the delete bookmarks

--- bimarks/app/helpers/sessions_helper.rb
+++ bimarks/app/helpers/sessions_helper.rb
@@ -15,6 +15,10 @@ module SessionsHelper
 	def current_user=(user)
 	  @current_user = user
 	end 
+	
+	def current_user?(user)
+    	  user == current_user
+  	end
 
 	def current_user
 	  @current_user ||= user_from_remember_token
