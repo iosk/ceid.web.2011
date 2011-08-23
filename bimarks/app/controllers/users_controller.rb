198231207058dce478838c5d4a4341cc6f204d3b
Ioannis Kotsogiannis <kotsogiann@ceid.upatras.gr>
2011-08-23 20:32:20 +0300

    fixed some bs i did.

--- bimarks/app/controllers/users_controller.rb
+++ bimarks/app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
 
+
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @users }
