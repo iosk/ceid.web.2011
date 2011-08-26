64ed4203258bde2e47d15a791d6600aa857ac4fb
Ioannis Kotsogiannis <kotsogiann@ceid.upatras.gr>
2011-08-27 01:49:01 +0300

    Tags are complete, or so I think so. ;)

--- bimarks/public/javascripts/application.js
+++ bimarks/public/javascripts/application.js
@@ -6,8 +6,15 @@ function remove_field(link) {
   $(link).prev("input[type=hidden]").val("1");
   $(link).closest(".fields").hide();
 }
+//########################################################
+//##       Add Tag links                   	            ##
+//########################################################
 
-
+function add_field(link, association, content) {
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g")
+  $(link).parent().before(content.replace(regexp, new_id));
+}
 
 //########################################################
 //##       Validation for USER Registration	        ##
