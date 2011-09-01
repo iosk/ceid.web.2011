cafbf671a3a2ebcf407a39bb4f4692640ed90dbc
Charalampos Kalargaris <kalargarisbabis@gmail.com>
2011-09-01 05:26:37 +0300

    All Buttons, At home page at tab==search some modification also.

--- bimarks/app/controllers/home_controller.rb
+++ bimarks/app/controllers/home_controller.rb
@@ -1,9 +1,9 @@
 class HomeController < ApplicationController
   def index
     @bookmarks_most_recent = Bookmark.find(:all, :order => "created_at DESC").first(10)
-    @bookmarks_top_rated = Bookmark.all.sort_by(&:avg_rating).reverse.first(10)
-    
+    @bookmarks_top_rated = Bookmark.all.sort_by(&:avg_rating).reverse.first(10) 
     @tag_unique = Tag.all.sort_by(&:count).first(20)
+    
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :action => "index" }
